be5d652fee2bbe2b6f7c615f016c993c65f0fb62
faraz9993 <farazansari9993@gmail.com>
2024-05-11 14:53:13 +0530

    Pushing 2_Exercise

---
+++ 2_Exercise.sh
@@ -0,0 +1,6 @@
+#Exercise 2:
+#Modify the shell script from exercise 1 to include a variable. 
+# The variable will hold the contents of the message "Shell Scripting is Fun!".
+
+var="Shell Script is Fun!"
+echo $var
