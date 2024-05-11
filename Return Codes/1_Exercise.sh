1bb85652ca35163fdecdec50f25bbf9a34007615
faraz9993 <farazansari9993@gmail.com>
2024-05-11 16:04:59 +0530

    Pushing the script of return codes

---
+++ Return Codes/1_Exercise.sh
@@ -0,0 +1,9 @@
+# Exercise 1:
+# Write a shell script that displays "This script will exit with a 0 exit status." 
+# Be sure that the script does indeed exit with a 0 exit status.
+
+#!/bin/bash
+echo "This script will exit with a 0 exit status."
+exit 0
+
+# run "echo $?" to check the exit code status. 
